74ca8ab69dfa621cb88367dde79f25a7cdf69b28
Paweł <pawel.kolendaa@gmail.com>
2024-03-03 00:46:07 +0100

    Fix routing for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import TrainingSegment from "./components/TrainingSegment";
+import { Routes, Route } from "react-router-dom";
+import Trainings from "./routes/Trainings";
 
 function App() {
-  const [trainingSegments, setTrainingSegments] = useState([1]);
-
-    const trainingSegmentsDisplay = trainingSegments.map(
-        (trainingSegmentIndex) => (
-            <TrainingSegment
-                key={trainingSegmentIndex}
-                index={trainingSegmentIndex}
-            />
-        )
-    );
-
-    function handleAddTrainingSegment() {
-        const newTrainingSegments = trainingSegments.concat(trainingSegments.length + 1);
-        setTrainingSegments(newTrainingSegments);
-    }
-
     return (
         <div className="p-4">
-            {trainingSegmentsDisplay}
-            <div><button onClick={handleAddTrainingSegment}>NADUŚ ABY DODAĆ</button></div>
+            <Routes>
+                <Route path="/" element={<Trainings />} />
+            </Routes>
         </div>
     );
 }
---
+++ src/routes/Trainings.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import TrainingSegment from "../components/TrainingSegment";
+
+function Trainings() {
+    const [trainingSegments, setTrainingSegments] = useState([1]);
+
+    const trainingSegmentsDisplay = trainingSegments.map(
+        (trainingSegmentIndex) => (
+            <TrainingSegment
+                key={trainingSegmentIndex}
+                index={trainingSegmentIndex}
+            />
+        )
+    );
+
+    function handleAddTrainingSegment() {
+        const newTrainingSegments = trainingSegments.concat(
+            trainingSegments.length + 1
+        );
+        setTrainingSegments(newTrainingSegments);
+    }
+
+    return (
+        <div className="p-4">
+            {trainingSegmentsDisplay}
+            <div>
+                <button onClick={handleAddTrainingSegment}>
+                    NADUŚ ABY DODAĆ
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Trainings;

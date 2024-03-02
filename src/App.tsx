3e722605c8f7ad7958bdd55d28835b176ca2f2d9
Paweł <pawel.kolendaa@gmail.com>
2024-03-02 23:38:31 +0100

    Create training segment draft component with ability to add many of them on main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TrainingSegment from "./components/TrainingSegment";
 
 function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+  const [trainingSegments, setTrainingSegments] = useState([1]);
+
+    const trainingSegmentsDisplay = trainingSegments.map(
+        (trainingSegmentIndex) => (
+            <TrainingSegment
+                key={trainingSegmentIndex}
+                index={trainingSegmentIndex}
+            />
+        )
+    );
+
+    function handleAddTrainingSegment() {
+        const newTrainingSegments = trainingSegments.concat(trainingSegments.length + 1);
+        setTrainingSegments(newTrainingSegments);
+    }
+
+    return (
+        <div className="App">
+            {trainingSegmentsDisplay}
+            <div><button onClick={handleAddTrainingSegment}>NADUŚ ABY DODAĆ</button></div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TrainingSegment.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+function TrainingSegment({ index }: any) {
+    function shouldBeExpanded() {
+        return index === 1;
+    }
+
+    return (
+        <Accordion
+            defaultExpanded={shouldBeExpanded()}
+            aria-controls={`training-segment-${index}`}
+            id={`training-segment-${index}`}
+        >
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={`training-segment-${index}-summary`}
+                id={`training-segment-${index}-summary`}
+            >
+                Segment {index}.
+            </AccordionSummary>
+            <AccordionDetails>
+                <p>Rodzaj treningu</p>
+                <p>Kilometry</p>
+                <p>Tętno</p>
+                <p>Tępo</p>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
+
+export default TrainingSegment;
